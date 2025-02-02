4c434901dda833ba61fb2c0084383c519c16aee1
LEGION-GO\hansung <imhansung91@gmail.com>
2025-02-02 22:06:56 +0900

    블로그 컨텐츠 수정 VO 작성하기

---
+++ src/main/java/v2/mvc/spring/blog/vo/BlogEditRequestVO.java
@@ -0,0 +1,28 @@
+package v2.mvc.spring.blog.vo;
+
+public class BlogEditRequestVO {
+
+	private int blogContSeq;
+	private String title;
+	private String contBdy;
+	
+	public int getBlogContSeq() {
+		return blogContSeq;
+	}
+	public void setBlogContSeq(int blogContSeq) {
+		this.blogContSeq = blogContSeq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContBdy() {
+		return contBdy;
+	}
+	public void setContBdy(String contBdy) {
+		this.contBdy = contBdy;
+	}
+
+}
