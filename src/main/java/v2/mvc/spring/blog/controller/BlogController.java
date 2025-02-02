5e29bee43ff2d3e0c94d31c9a6a0f63450d3329b
LEGION-GO\hansung <imhansung91@gmail.com>
2025-02-02 21:07:41 +0900

    컨트롤러에 서비스 주입

--- src/main/java/v2/mvc/spring/blog/controller/BlogController.java
+++ src/main/java/v2/mvc/spring/blog/controller/BlogController.java
@@ -1,11 +1,18 @@
 package v2.mvc.spring.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import vc.mvc.spring.blog.service.BlogService;
+
 @Controller
 public class BlogController {
+	
+	@Autowired
+	BlogService blogService;
+	
 	@RequestMapping(value="/create", method = RequestMethod.GET)
 	public String create() {
 		return "blog/create";
