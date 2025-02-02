e38aae0dd2a2a314ff35ff4cceafa44f69324313
LEGION-GO\hansung <imhansung91@gmail.com>
2025-02-02 21:02:22 +0900

    서비스 클래스 추가

---
+++ src/main/java/vc/mvc/spring/blog/service/impl/BlogServiceImpl.java
@@ -0,0 +1,15 @@
+package vc.mvc.spring.blog.service.impl;
+
+import java.util.Map;
+
+import vc.mvc.spring.blog.service.BlogService;
+
+public class BlogServiceImpl implements BlogService {
+
+	@Override
+	public int create(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
