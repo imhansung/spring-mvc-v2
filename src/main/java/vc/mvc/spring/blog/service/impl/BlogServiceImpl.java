dd7f25436393457dee80e2f32b503966d971657b
LEGION-GO\hansung <imhansung91@gmail.com>
2025-02-02 21:06:33 +0900

    서비스 어노테이션 붙이기

--- src/main/java/vc/mvc/spring/blog/service/impl/BlogServiceImpl.java
+++ src/main/java/vc/mvc/spring/blog/service/impl/BlogServiceImpl.java
@@ -3,10 +3,12 @@ package vc.mvc.spring.blog.service.impl;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import vc.mvc.spring.blog.dao.BlogDAO;
 import vc.mvc.spring.blog.service.BlogService;
 
+@Service
 public class BlogServiceImpl implements BlogService {
 
 	private BlogDAO blogDAO;
