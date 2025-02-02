7d4743ca3eaba21e3c46b89de45288cd4861fb25
LEGION-GO\hansung <imhansung91@gmail.com>
2025-02-02 18:35:20 +0900

    DAO 클래스에 insert 메서드 작성  - return 부분 cast 문제 수정(chat gpt)

--- src/main/java/vc/mvc/spring/blog/dao/BlogDAO.java
+++ src/main/java/vc/mvc/spring/blog/dao/BlogDAO.java
@@ -1,11 +1,24 @@
 package vc.mvc.spring.blog.dao;
 
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BlogDAO {
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
+	
+	public int insert(Map<String, Object> map) {
+	    int result = this.sqlSessionTemplate.insert("TB_BLG_CONT.insert", map);
+	    if (result > 0 && map.containsKey("seq_blg_cont")) {
+	    	Object obj = map.get("seq_blg_cont");
+	    	return obj instanceof Number ? ((Number) obj).intValue() : -1;
+	    }
+	 
+	    return -1;
+	}
 
 }
