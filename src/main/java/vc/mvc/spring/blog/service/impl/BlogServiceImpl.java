8ddb68b4958244f4a91e44578420f34968f15da1
LEGION-GO\hansung <imhansung91@gmail.com>
2025-02-02 21:04:52 +0900

    서비스 생성자 주입

--- src/main/java/vc/mvc/spring/blog/service/impl/BlogServiceImpl.java
+++ src/main/java/vc/mvc/spring/blog/service/impl/BlogServiceImpl.java
@@ -2,10 +2,20 @@ package vc.mvc.spring.blog.service.impl;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import vc.mvc.spring.blog.dao.BlogDAO;
 import vc.mvc.spring.blog.service.BlogService;
 
 public class BlogServiceImpl implements BlogService {
 
+	private BlogDAO blogDAO;
+	
+	@Autowired
+	public BlogServiceImpl(BlogDAO blogDAO) {
+	    this.blogDAO = blogDAO;
+	}
+	
 	@Override
 	public int create(Map<String, Object> map) {
 		// TODO Auto-generated method stub
