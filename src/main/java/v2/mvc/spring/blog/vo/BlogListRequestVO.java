62a0d488689da468ab7dfa3b4caee3f0af4ecdf8
DESKTOP-IO87GKT\CURONSYS <hsim@curonsys.com>
2025-02-03 14:56:56 +0900

    블로그 컨텐츠 목록 요청 VO 만들기

---
+++ src/main/java/v2/mvc/spring/blog/vo/BlogListRequestVO.java
@@ -0,0 +1,15 @@
+package v2.mvc.spring.blog.vo;
+
+public class BlogListRequestVO {
+	private String search;
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+	 
+
+}
