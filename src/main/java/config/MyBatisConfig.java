9a9890e85bcc3c9502f8fa6f52b74dcf3213ef24
DESKTOP-IO87GKT\CURONSYS <hsim@curonsys.com>
2025-02-03 14:56:40 +0900

    게시글 삭제 매퍼 작성  - chat gpt, 블로그 참조  - (https://steady-record.tistory.com/entry/Spring-MyBatis-%EC%99%80-Spring-%EC%97%B0%EB%8F%99%ED%95%98%EA%B8%B03-Interface-Mapper%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%9C-%EC%97%B0%EB%8F%99)  - 삭제 장상작동 되는지 확인 필요

---
+++ src/main/java/config/MyBatisConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("v2.mvc.spring.blog.mapper")  // 👈 Mapper 인터페이스 패키지 지정
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
+    }
+}
--- src/main/java/v2/mvc/spring/blog/service/impl/BlogServiceImpl.java
+++ src/main/java/v2/mvc/spring/blog/service/impl/BlogServiceImpl.java
@@ -14,6 +14,7 @@ import v2.mvc.spring.blog.vo.BlogEditRequestVO;
 public class BlogServiceImpl implements BlogService {
 
 	private BlogDAO blogDAO;
+	//@Autowired
 	private BlogMapper blogMapper;
 	
 	@Autowired
